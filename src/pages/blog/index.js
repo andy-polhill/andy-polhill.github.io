1af601d650d12b6ffea325a03a755f172c932b01
Andy Polhill <2196085+andy-polhill@users.noreply.github.com>
2022-02-19 08:49:02 +0000

    fix(blog): correctly sort blog index

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -51,7 +51,7 @@ export const pageQuery = graphql`
   query MyQuery {
     blog: allMdx(
         filter: { fileAbsolutePath: {regex: "/(blog)/"  }}
-        sort: { fields: [frontmatter___date, frontmatter___title], order: ASC }
+        sort: { fields: [frontmatter___date, frontmatter___title], order: DESC }
       ) {
       posts: nodes {
         frontmatter {
@@ -66,42 +66,3 @@ export const pageQuery = graphql`
     }
   }
 `
-
-// import * as React from 'react'
-// import { Link, graphql } from 'gatsby'
-
-// const BlogPage = ({ data }) => {
-//   return (
-//     <div>
-//       {/* {
-//         data.allMdx.nodes.map(node => (
-//           <article key={node.id}>
-//             <h2>
-//               <Link to={`/blog/${node.slug}`}>
-//                 {node.frontmatter.title}
-//               </Link>
-//             </h2>
-//             <p>Posted: {node.frontmatter.date}</p>
-//           </article>
-//         ))
-//       } */}
-//     </div>
-//   )
-// }
-
-// export const query = graphql`
-//   query {
-//     allMdx(sort: {fields: frontmatter___date, order: DESC}) {
-//       nodes {
-//         frontmatter {
-//           date(formatString: "MMMM D, YYYY")
-//           title
-//         }
-//         id
-//         slug
-//       }
-//     }
-//   }
-// `
-
-// export default BlogPage
